9e63fc1e79c76ca1fd9bf25275a63cd3261cb2f3
Sebastian Espiritu <sebastian.espiritu@tecsup.edu.pe>
2026-09-03 08:49:44 -0500

    Crear app veterinaria y definir modelo de datos estaticos

---
+++ sesion1/veterinaria/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+CITAS_DB = [
+    {'id': 1, 'mascota': 'Fiddo', 'propietario': 'Carlos Gómez', 'tipo_atencion': 'Vacunación', 'fecha': '2026-09-01', 'hora': '10:00'},
+    {'id': 2, 'mascota': 'Daki', 'propietario': 'María López', 'tipo_atencion': 'Consulta General', 'fecha': '2026-09-01', 'hora': '11:30'},
+    {'id': 3, 'mascota': 'Ñaño', 'propietario': 'Juan Pérez', 'tipo_atencion': 'Desparasitación', 'fecha': '2026-09-02', 'hora': '09:00'},
+    {'id': 4, 'mascota': 'Pelusa', 'propietario': 'Ana Martínez', 'tipo_atencion': 'Cirugía', 'fecha': '2026-09-02', 'hora': '15:00'},
+    {'id': 5, 'mascota': 'Rocky', 'propietario': 'Luis Torres', 'tipo_atencion': 'Baño y Corte', 'fecha': '2026-09-03', 'hora': '16:00'},
+]
